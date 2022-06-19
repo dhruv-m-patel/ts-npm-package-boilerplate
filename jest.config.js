515bec50dbcf2b9bf5cc1d4da4835a28f5c4d9e1
Dhruv Patel <dhruv.codelab@gmail.com>
2022-06-19 16:45:40 +0530

    feat: v0.0.1

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  globals: {
+    NODE_ENV: 'test',
+  },
+  clearMocks: true,
+  verbose: true,
+  testEnvironment: 'node',
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  coveragePathIgnorePatterns: ['/node_modules/', path.join(__dirname, 'build')],
+  testMatch: [
+    path.join(__dirname, 'tests/**/*.test.ts'),
+     path.join(__dirname, 'tests/**/*.test.tsx'),
+    path.join(__dirname, 'src/**/*.test.ts'),
+    path.join(__dirname, 'src/**/*.test.tsx'),
+  ],
+  rootDir: __dirname,
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+# ts-npm-package-boilerplate
+
+A boilerplate node-package for writing reusable package for services
